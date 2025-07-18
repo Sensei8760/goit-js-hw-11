7162d4c2d6b508e9f505204ec4eb8f588a687997
sensei <gnid.yura@gmail.com>
2025-07-18 21:44:03 +0300

    add-task

---
+++ src/js/render-functions.js
@@ -0,0 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+export function createGallery(images) {
+  const markup = images
+    .map(image => {
+      return `
+        <li class="gallery-item">
+          <a href="${image.largeImageURL}">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+          </a>
+          <div class="info">
+            <p>👍 ${image.likes}</p>
+            <p>👁 ${image.views}</p>
+            <p>💬 ${image.comments}</p>
+            <p>⬇️ ${image.downloads}</p>
+          </div>
+        </li>
+      `;
+    })
+    .join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryContainer.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.add('is-visible');
+}
+
+export function hideLoader() {
+  loader.classList.remove('is-visible');
+}
